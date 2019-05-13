2bfef0594e85a0e6ac58ba618862f5664d4417a7
Olivier JM <manolivier93@gmail.com>
2019-05-13 17:38:20 +0200

    refactored the header

--- src/Pages/Articles.js
+++ src/Pages/Articles.js
@@ -7,58 +7,25 @@ function Articles() {
   return (
     <div>
       {/* <div>
-        <Header transparent="" />
       </div> */}
 
-      <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
-        <div className="uk-position-relative">
-          <div className="uk-position-top">
-            <nav className="uk-navbar-container" uk-navbar="true">
-              <div className="uk-navbar-left">
-                <ul className="uk-navbar-nav">
-                  <li className="uk-active">
-                    <a href="#">Active</a>
-                  </li>
-                  <li>
-                    <a href="#">Parent</a>
-                    <div className="uk-navbar-dropdown">
-                      <ul className="uk-nav uk-navbar-dropdown-nav">
-                        <li className="uk-active">
-                          <a href="#">Active</a>
-                        </li>
-                        <li>
-                          <a href="#">Item</a>
-                        </li>
-                        <li>
-                          <a href="#">Item</a>
-                        </li>
-                      </ul>
-                    </div>
-                  </li>
-                  <li>
-                    <a href="#">Item</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </div>
+      <Header />
+      <div className="uk-container">
+        <div>
+          <h1 className="uk-heading-line uk-text-center">
+            <span style={{ marginTop: "12%" }}>Here are different blogs </span>
+          </h1>
         </div>
-      </div>
-
-      <div>
-        <h1 className="uk-heading-line uk-text-center">
-          <span style={{ marginTop: "10%" }}>Here are different blogs </span>
-        </h1>
-      </div>
-      <div uk-grid="true">
-        {blogs.map(blog => (
-          <div key={blog.id} className="uk-width-1-1">
-            <div className="uk-card uk-card-default uk-card-body">
-              <h3 className="uk-card-title">{blog.title}</h3>
-              <p>{blog.description}</p>
+        <div uk-grid="true">
+          {blogs.map(blog => (
+            <div key={blog.id} className="uk-width-1-1">
+              <div className="uk-card uk-card-default uk-card-body">
+                <h3 className="uk-card-title">{blog.title}</h3>
+                <p>{blog.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,25 @@
 import React from "react";
 
 // uk-navbar-transparent
-function Header({ transparent }) {
+function Header() {
   return (
     <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
       <div className="uk-position-relative">
         <div className="uk-position-top">
           <nav
-            className={`uk-navbar-container ${transparent}`}
+            className="uk-navbar-container uk-navbar-transparent"
             uk-navbar="true"
           >
             <div className="uk-navbar-left">
-              <ul className="uk-navbar-nav ">
-                <li>
-                  <a href="/">Projects</a>
+              <ul className="uk-navbar-nav">
+                <li className="uk-active">
+                  <a href="#">Active</a>
                 </li>
                 <li>
-                  <a href="/">Beyond Screen</a>
+                  <a href="#">Projects</a>
+                </li>
+                <li>
+                  <a href="#">Articles</a>
                 </li>
               </ul>
             </div>
