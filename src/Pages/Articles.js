43df2c4207196bc29c807aaa6f703330e6aebf0d
Olivier JM <manolivier93@gmail.com>
2019-05-13 17:33:44 +0200

    added some margins on the sticky header

--- src/Pages/Articles.js
+++ src/Pages/Articles.js
@@ -1,14 +1,55 @@
 import React from "react";
 import * as data from "../blogs.json";
+import Header from "../components/Header";
 
 function Articles() {
   const blogs = data.default;
   return (
     <div>
-      {/* <h4 className="uk-heading-medium" /> */}
-      <h1 class="uk-heading-line uk-text-center">
-        <span>Here are different blogs I have written in recent years</span>
-      </h1>
+      {/* <div>
+        <Header transparent="" />
+      </div> */}
+
+      <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
+        <div className="uk-position-relative">
+          <div className="uk-position-top">
+            <nav className="uk-navbar-container" uk-navbar="true">
+              <div className="uk-navbar-left">
+                <ul className="uk-navbar-nav">
+                  <li className="uk-active">
+                    <a href="#">Active</a>
+                  </li>
+                  <li>
+                    <a href="#">Parent</a>
+                    <div className="uk-navbar-dropdown">
+                      <ul className="uk-nav uk-navbar-dropdown-nav">
+                        <li className="uk-active">
+                          <a href="#">Active</a>
+                        </li>
+                        <li>
+                          <a href="#">Item</a>
+                        </li>
+                        <li>
+                          <a href="#">Item</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <a href="#">Item</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h1 className="uk-heading-line uk-text-center">
+          <span style={{ marginTop: "10%" }}>Here are different blogs </span>
+        </h1>
+      </div>
       <div uk-grid="true">
         {blogs.map(blog => (
           <div key={blog.id} className="uk-width-1-1">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Header() {
+// uk-navbar-transparent
+function Header({ transparent }) {
   return (
     <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
       <div className="uk-position-relative">
         <div className="uk-position-top">
           <nav
-            className="uk-navbar-container uk-navbar-transparent"
+            className={`uk-navbar-container ${transparent}`}
             uk-navbar="true"
           >
             <div className="uk-navbar-left">
