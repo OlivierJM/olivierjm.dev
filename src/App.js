76f033b3c73251b545bc50204c1299bdd20cbaba
OlivierJM <manolivier93@gmail.com>
2019-12-16 20:13:58 +0200

    removed the old ui

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-// components
-import Parallax from "./components/Parralax";
-import Cards from "./components/Cards";
-import Header from "./components/Header";
-
-// pages
-import Articles from "./Pages/Articles";
-import Notfound from "./Pages/Notfound";
-import Blog from "./Pages/Blog";
-// import Head
-
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/articles" component={Articles} />
-        <Route path="/blog" component={Blog} />
-        <Route path="*" component={Notfound} />
+        <Route
+          exact
+          path="/"
+          component={<h4>Hello I am the home page guy</h4>}
+        />
       </Switch>
     </BrowserRouter>
-  );
-}
-
-function Home() {
-  return (
-    <>
-      <Header />
-      <Parallax />
-      <Cards />
-    </>
   );
 }
 
--- src/Pages/Notfound.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function Notfound() {
-  return <div>I am a Notfound Page</div>;
-}
-export default Notfound;
