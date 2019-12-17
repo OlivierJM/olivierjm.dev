1c9b9dad89ccf61ebef7c590787bca054691da50
OlivierJM <manolivier93@gmail.com>
2019-12-17 23:06:23 +0200

    formatted files and added formatter to github actions

--- src/pages/info.js
+++ src/pages/info.js
@@ -9,25 +9,31 @@ export default function Info() {
     <Layout page="info" bgColor={infoData.background_color}>
       <section className={infoStyles.info_blurb}>
         <h2>
-          <div dangerouslySetInnerHTML={{__html: infoData.description}}></div>
-          <div dangerouslySetInnerHTML={{__html: infoData.cta}}></div>
+          <div dangerouslySetInnerHTML={{ __html: infoData.description }}></div>
+          <div dangerouslySetInnerHTML={{ __html: infoData.cta }}></div>
         </h2>
         <ul>
           <li>
             <p>
-              <a href={`mailto:${infoData.contact.email}`}>Email: {infoData.contact.email}</a>
+              <a href={`mailto:${infoData.contact.email}`}>
+                Email: {infoData.contact.email}
+              </a>
             </p>
           </li>
           <li>
             <p>
-              <a href={`https://twitter.com/${infoData.contact.twitter_handle}`}>
+              <a
+                href={`https://twitter.com/${infoData.contact.twitter_handle}`}
+              >
                 Twitter: @{infoData.contact.twitter_handle}
               </a>
             </p>
           </li>
           <li>
             <p>
-              <a href={`https://github.com/${infoData.contact.github_handle}`}>Github: {infoData.contact.github_handle}</a>
+              <a href={`https://github.com/${infoData.contact.github_handle}`}>
+                Github: {infoData.contact.github_handle}
+              </a>
             </p>
           </li>
         </ul>
