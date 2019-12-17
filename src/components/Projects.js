02b3ac65e62a204f9a84556f2588603c88d402f2
OlivierJM <manolivier93@gmail.com>
2019-12-17 23:05:01 +0200

    modified the view home

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,9 @@ export default function BlogList() {
       <div className="cell medium-9 medium-cell-block-y">
         <div class="grid-container">
           <div class="grid-x grid-margin-x small-up-1 medium-up-2 large-up-3">
-            <h4 className="text-center subheader">Welcome to Olivierjm.dev</h4>
+            <h4 style={{ margin: "30%" }} className="text-center subheader">
+              Welcome to Olivierjm.dev
+            </h4>
           </div>
         </div>
       </div>
